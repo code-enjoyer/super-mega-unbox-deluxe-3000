bc59e15177965ef170b773011004501c5a2b017d
code-enjoyer <96414969+code-enjoyer@users.noreply.github.com>
2026-09-12 16:47:52 +1000

    Added get endpoint

--- api/SuperMegaUnboxDeluxe.Api/Controllers/ItemsController.cs
+++ api/SuperMegaUnboxDeluxe.Api/Controllers/ItemsController.cs
@@ -1,18 +1,18 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 
 namespace SuperMegaUnboxDeluxe.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ItemsController
+public class ItemsController : ControllerBase
 {
-    [HttpPost(Name = "GenerateItem")]
-    public async Task<string> GenerateItemAsync([FromBody] ItemRequest? request, CancellationToken cancellationToken)
+    [HttpGet("{id:guid}", Name = "GetItemDetails")]
+    public async Task<ActionResult<Item>> GetItemDetailsAsync(Guid id, CancellationToken cancellationToken)
     {
         var item = new Item
         {
+            Id = id,
             ItemBase = "Sword",
             Name = "Excalibur",
             Stats = new[] { "Attack: 100", "Durability: 80" },
@@ -23,7 +23,28 @@ public class ItemsController
             Value = "1000 Gold"
         };
 
-        return JsonSerializer.Serialize(item);
+        return Ok(item);
+    }
+
+    [HttpPost(Name = "GenerateItem")]
+    public async Task<ActionResult<Item>> GenerateItemAsync([FromBody] ItemRequest? request, CancellationToken cancellationToken)
+    {
+        var item = new Item
+        {
+            Id = Guid.NewGuid(),
+            ItemBase = "Sword",
+            Name = "Excalibur",
+            Stats = new[] { "Attack: 100", "Durability: 80" },
+            Modifiers = new[] { "+10% Critical Hit Chance", "+5% Attack Speed" },
+            Rarity = "Legendary",
+            ImageKey = "excalibur",
+            DateGotten = DateTime.UtcNow.ToString("yyyy-MM-dd"),
+            Value = "1000 Gold"
+        };
+
+        return CreatedAtAction("GetItemDetails",
+            new { id = item.Id },
+            item);
     }
 
     public class ItemRequest
@@ -33,6 +54,7 @@ public class ItemsController
 
     public class Item
     {
+        public Guid Id { get; set; }
         public string ItemBase { get; init; }
         public string Name { get; init; }
         public string[] Stats { get; init; }
